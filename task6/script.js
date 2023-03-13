5eda33fdd039540e1f92f1e760f3b7cdd8d7474a
Marina Malahova <dalid@inbox.ru>
2023-03-13 20:56:41 +0300

    change traffic-light

--- task6/script.js
+++ task6/script.js
@@ -1,45 +1,52 @@
+
+
 const Green = document.querySelector('#circle1');
-//Green.addEventListener('click', makeGreen);
+Green.addEventListener('click', makeGreen);
 
 const Yellow = document.querySelector('#circle2');
-//Yellow.addEventListener('click', makeYellow);
+Yellow.addEventListener('click', makeYellow);
 
 const Red = document.querySelector('#circle3');
-//Red.addEventListener('click', makeRed);
-
-const Black = document.querySelector('.one');
-Black.addEventListener('click', makeBlack);
+Red.addEventListener('click', makeRed);
 
 
 function makeGreen() {
     Green.style.background = ('green');
-    Green.removeEventListener('click', makeGreen);
-    Green.addEventListener('click', makeYellow);
+
+   // Green.removeEventListener('click', makeGreen);
+    //Green.addEventListener('click', makeYellow);
    // Green.addEventListener('click', makeRed);
 } 
 
-Green.addEventListener('click', makeGreen);
+//Green.addEventListener('click', makeGreen);
 
 function makeYellow() {
     Yellow.style.background = ('yellow');
-    Yellow.removeEventListener('click', makeYellow);
-    Yellow.addEventListener('click', makeRed);
+   //Yellow.removeEventListener('click', makeYellow);
+    //Yellow.addEventListener('click', makeRed);
 }
-Yellow.addEventListener('click', makeYellow);
+//Yellow.addEventListener('click', makeYellow);
 
 function makeRed() {
     Red.style.background = ('red');
-    Red.removeEventListener('click', makeRed);
-    Red.addEventListener('click', makeGreen);
-  
+    //Red.removeEventListener('click', makeRed);
+    //Red.addEventListener('click', makeGreen);
 }
-Red.addEventListener('click', makeRed);
+//Red.addEventListener('click', makeRed);
 
-function makeBlack() {
-    Black.style.background = ('black');
-    Black.removeEventListener('click', makeBlack);
-    
-}
-Black.addEventListener('click', makeBlack);
-
-
+//const color = document.addEventListener('.color', function(){
+    if(Green.makeGreen ) {
+        Green = Green.makeGreen();
+    }
+    if (Yellow.makeGreen) {
+        Yellow = Yellow.makeYellow;
+        Green.removeEventListener('click', makeGreen);
+        Green = nyll;
+    }
+    if (Red.makeRed) {
+        Red = Red.makeRed;
+        Yellow.removeEventListener('click', makeYellow);
+        Yellow = nyll;
+    }    
+        
+    //})
